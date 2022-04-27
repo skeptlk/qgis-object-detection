a062a4114e66dd9de0d3b9954d7a4c13fd968a88
Bogdan <sceptlc@bk.ru>
2022-04-27 14:21:57 +0300

    First commit

---
+++ object_detector_plugin_dialog.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from qgis.PyQt import uic
+from qgis.PyQt import QtWidgets
+from qgis.PyQt.QtWidgets import QFileDialog
+
+
+# This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'object_detector_plugin_dialog_base.ui'))
+
+
+class ObjectDetectorPluginDialog(QtWidgets.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(ObjectDetectorPluginDialog, self).__init__(parent)
+        self.setupUi(self)
+        self.selectFileButton.clicked.connect(self.select_output_file)
+
+    def set_layers(self, layers):
+        self.layers = layers
+        self.layerComboBox.clear()
+        # # Populate the comboBox with names of all the loaded layers
+        self.layerComboBox.addItems([layer.name() for layer in layers])
+
+    def select_output_file(self):
+        filename, _filter = QFileDialog.getSaveFileName(
+            self, "Select output file", "", "*.csv"
+        )
+        self.filename = filename
+        self.fileNameLineEdit.setText(filename)
+
+    def get_selected_layer(self):
+        selectedLayerIndex = self.layerComboBox.currentIndex()
+        return self.layers[selectedLayerIndex].layer()
